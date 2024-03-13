05b204e3119e7f3e005dffc8bc900d850683d858
du <2637599327@qq.com>
2024-03-13 19:50:12 +0800

    新增member会员模块1

--- member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
+++ member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.jiawa")
 public class MemberApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(MemberApplication.class, args);
     }
