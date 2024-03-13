150fa6e9cb63d23b23634816f221369c5bf3541a
du <2637599327@qq.com>
2024-03-13 18:11:57 +0800

    新增member会员模块

---
+++ member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
@@ -0,0 +1,16 @@
+package com.jiawa.train.member.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//默认扫描当前包及其子孙包的所有类，扫描不到外面，加@ComponentScan
+@ComponentScan("com.jiawa")
+public class MemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+
+}
