00cd8982ff6e3c96d3e114dd3d412aca998d4bc4
du <2637599327@qq.com>
2024-03-13 22:29:47 +0800

    4.4修改启动日志，增加/member,修改logback,忽略不用提交的文件/log

--- member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
+++ member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
@@ -1,17 +1,25 @@
 package com.jiawa.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 //默认扫描当前包及其子孙包的所有类，扫描不到外面，加@ComponentScan
 @ComponentScan("com.jiawa")
 public class MemberApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        //可以打印欢迎语句，口号等，或者项目常用地址，比如接口文档，项目文档，数据库文档
+        LOG.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
     }
 
 }
