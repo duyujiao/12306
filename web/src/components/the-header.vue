90521ebbe164b4e9045083069095899fc7c77f65
du <2637599327@qq.com>
2024-04-07 23:15:02 +0800

    7.6 实现菜单同步激活

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -8,7 +8,7 @@
       </router-link>
     </div>
     <a-menu
-        v-model:selectedKeys="selectedKeys1"
+        v-model:selectedKeys="selectedKeys"
         theme="dark"
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
@@ -28,17 +28,24 @@
 </template>
 
 <script>
-import {defineComponent, ref} from 'vue';
+import {defineComponent, ref, watch} from 'vue';
 import store from "@/store";
+import router from '@/router'
 
 export default defineComponent({
   name: "the-header-view",
   setup() {
     let member = store.state.member;
+    const selectedKeys = ref([]);
 
+    watch(() => router.currentRoute.value.path, (newValue) => {
+      console.log('watch', newValue);
+      selectedKeys.value = [];
+      selectedKeys.value.push(newValue);
+    }, {immediate: true});
     return {
-      selectedKeys1: ref(['2']),
-      member
+      member,
+      selectedKeys
     };
   },
 });
@@ -46,5 +53,11 @@ export default defineComponent({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.logo {
+  float: left;
+  height: 31px;
+  width: 150px;
+  color: white;
+  font-size: 20px;
+}
 </style>
