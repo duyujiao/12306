e47d3dfd03ff3e3ccb4a90a9569fbab760068b4d
du <2637599327@qq.com>
2024-04-07 21:50:17 +0800

    7.3 增加乘车人新增接口,增加字段非空校验

--- member/src/main/java/com/jiawa/train/member/req/PassengerSaveReq.java
+++ member/src/main/java/com/jiawa/train/member/req/PassengerSaveReq.java
@@ -1,20 +1,52 @@
 package com.jiawa.train.member.req;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.Date;
 
 public class PassengerSaveReq {
+
+    /**
+     * id
+     */
     private Long id;
 
+    /**
+     * 会员id
+     */
+    @NotNull(message="【会员id】不能为空")
     private Long memberId;
 
+    /**
+     * 姓名
+     */
+    @NotBlank(message = "【姓名】不能为空")
     private String name;
 
+    /**
+     * 身份证
+     */
+    @NotBlank(message = "【身份证】不能为空")
     private String idCard;
 
+    /**
+     * 旅客类型|枚举[PassengerTypeEnum]
+     */
+    @NotBlank(message = "【旅客类型】不能为空")
     private String type;
 
+    /**
+     * 新增时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
+    /**
+     * 修改时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateTime;
 
     public Long getId() {
@@ -90,3 +122,4 @@ public class PassengerSaveReq {
         return sb.toString();
     }
 }
+
