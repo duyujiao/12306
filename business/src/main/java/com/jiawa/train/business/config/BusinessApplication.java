724e7379bb9ccb679bd98d1c39a4cb18f791be80
du <2637599327@qq.com>
2024-04-10 20:54:43 +0800

    9.4 增加business业务模块

--- business/src/main/java/com/jiawa/train/business/config/BusinessApplication.java
+++ business/src/main/java/com/jiawa/train/business/config/BusinessApplication.java
@@ -1,4 +1,4 @@
-package com.jiawa.train.member.config;
+package com.jiawa.train.business.config;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
@@ -9,18 +9,16 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-//默认扫描当前包及其子孙包的所有类，扫描不到外面，加@ComponentScan
 @ComponentScan("com.jiawa")
 @MapperScan("com.jiawa.train.*.mapper")
-public class MemberApplication {
+public class BusinessApplication {
 
-    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+    private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(MemberApplication.class);
+        SpringApplication app = new SpringApplication(BusinessApplication.class);
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功！！");
-        //可以打印欢迎语句，口号等，或者项目常用地址，比如接口文档，项目文档，数据库文档
         LOG.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
     }
 }
