fcd3e864bd4b5d996d1f46722d2f1aa4e9e43301
du <2637599327@qq.com>
2024-04-27 20:24:07 +0800

    11.8 集成openfeign,实现服务间调用，给每个应用增加应用名，可搭配配置中心使用

--- batch/src/main/java/com/jiawa/train/batch/controller/TestController.java
+++ batch/src/main/java/com/jiawa/train/batch/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.jiawa.train.batch.controller;
 
+import com.jiawa.train.batch.feign.BusinessFeign;
+import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,12 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Resource
+    BusinessFeign businessFeign;
 
     private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
 
-
     @GetMapping("/hello")
     public String hello() {
+        String businesshello = businessFeign.hello();
+        LOG.info(businesshello);
         return "Hello World! Batch!";
     }
 }
