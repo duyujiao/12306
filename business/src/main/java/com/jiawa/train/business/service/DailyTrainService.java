52bdda214987043d2099f6242244da172557bd0c
du <2637599327@qq.com>
2024-05-21 22:19:45 +0800

    11.12 增加生成每日座位功能

--- business/src/main/java/com/jiawa/train/business/service/DailyTrainService.java
+++ business/src/main/java/com/jiawa/train/business/service/DailyTrainService.java
@@ -37,6 +37,8 @@ public class DailyTrainService {
     private DailyTrainStationService dailyTrainStationService;
     @Resource
     private DailyTrainCarriageService dailyTrainCarriageService;
+    @Resource
+    private DailyTrainSeatService dailyTrainSeatService;
 
 
 
@@ -128,9 +130,9 @@ public class DailyTrainService {
 
         // 生成该车次的车厢数据
         dailyTrainCarriageService.genDaily(date, train.getCode());
-//
-//        // 生成该车次的座位数据
-//        dailyTrainSeatService.genDaily(date, train.getCode());
+
+        // 生成该车次的座位数据
+        dailyTrainSeatService.genDaily(date, train.getCode());
 //
 //        // 生成该车次的余票数据
 //        dailyTrainTicketService.genDaily(dailyTrain, date, train.getCode());
