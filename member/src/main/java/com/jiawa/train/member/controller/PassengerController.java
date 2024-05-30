b602c0b01f6f2b3d6a48370eb1f17ba0362f3cbd
du <2637599327@qq.com>
2024-05-30 20:53:02 +0800

    12.8 订票页面，查询所有的乘客，后端接口

--- member/src/main/java/com/jiawa/train/member/controller/PassengerController.java
+++ member/src/main/java/com/jiawa/train/member/controller/PassengerController.java
@@ -11,6 +11,8 @@ import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/passenger")
 public class PassengerController {
@@ -36,11 +38,11 @@ public class PassengerController {
         passengerService.delete(id);
         return new CommonResp<>();
     }
-//
-//    @GetMapping("/query-mine")
-//    public CommonResp<List<PassengerQueryResp>> queryMine() {
-//        List<PassengerQueryResp> list = passengerService.queryMine();
-//        return new CommonResp<>(list);
-//    }
+
+    @GetMapping("/query-mine")
+    public CommonResp<List<PassengerQueryResp>> queryMine() {
+        List<PassengerQueryResp> list = passengerService.queryMine();
+        return new CommonResp<>(list);
+    }
 
 }
