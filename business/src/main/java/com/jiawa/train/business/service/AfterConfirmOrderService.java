60666637662c3b8bf27db681819e7a8664881d70
du <2637599327@qq.com>
2024-08-09 16:51:22 +0800

    16 去掉seata依赖

--- business/src/main/java/com/jiawa/train/business/service/AfterConfirmOrderService.java
+++ business/src/main/java/com/jiawa/train/business/service/AfterConfirmOrderService.java
@@ -11,7 +11,6 @@ import com.jiawa.train.business.mapper.cust.DailyTrainTicketMapperCust;
 import com.jiawa.train.business.req.ConfirmOrderTicketReq;
 import com.jiawa.train.common.req.MemberTicketReq;
 import com.jiawa.train.common.resp.CommonResp;
-import io.seata.core.context.RootContext;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -47,7 +46,7 @@ public class AfterConfirmOrderService {
     // @Transactional
      //@GlobalTransactional
     public void afterDoConfirm(DailyTrainTicket dailyTrainTicket, List<DailyTrainSeat> finalSeatList, List<ConfirmOrderTicketReq> tickets, ConfirmOrder confirmOrder) throws Exception {
-         LOG.info("seata全局事务ID: {}", RootContext.getXID());
+         //LOG.info("seata全局事务ID: {}", RootContext.getXID());
         for (int j = 0; j < finalSeatList.size(); j++) {
             DailyTrainSeat dailyTrainSeat = finalSeatList.get(j);
             DailyTrainSeat seatForUpdate = new DailyTrainSeat();
