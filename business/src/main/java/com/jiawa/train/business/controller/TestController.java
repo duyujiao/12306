bbeb1f327c73b9531ce79eee1de46c2cad6cde83
du <2637599327@qq.com>
2024-08-10 01:14:02 +0800

    17 Sentinel+Feign熔断

--- business/src/main/java/com/jiawa/train/business/controller/TestController.java
+++ business/src/main/java/com/jiawa/train/business/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.jiawa.train.business.controller;
 
-import cn.hutool.core.util.RandomUtil;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.jiawa.train.business.service.TestService;
 import jakarta.annotation.Resource;
@@ -15,10 +14,11 @@ public class TestController {
     @SentinelResource("hello")
     @GetMapping("/hello")
     public String hello() throws InterruptedException {
-        int i = RandomUtil.randomInt(1, 10);
-        if (i <= 2) {
-            throw new RuntimeException("测试异常");
-        }
+        testService.hello2();
+//        int i = RandomUtil.randomInt(1, 10);
+//        if (i <= 2) {
+//            throw new RuntimeException("测试异常");
+//        }
         return "Hello World! Business!";
     }
     @SentinelResource("hello1")
