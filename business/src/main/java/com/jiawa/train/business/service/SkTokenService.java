027bea667d2c07195b94103f2a103bf72c486b00
du <2637599327@qq.com>
2024-08-11 00:31:58 +0800

    18 对放入redis的localKey加一个前缀，避免不同的业务共用一个

--- business/src/main/java/com/jiawa/train/business/service/SkTokenService.java
+++ business/src/main/java/com/jiawa/train/business/service/SkTokenService.java
@@ -8,6 +8,7 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.jiawa.train.business.domain.SkToken;
 import com.jiawa.train.business.domain.SkTokenExample;
+import com.jiawa.train.business.enums.RedisKeyPreEnum;
 import com.jiawa.train.business.mapper.SkTokenMapper;
 import com.jiawa.train.business.mapper.cust.SkTokenMapperCust;
 import com.jiawa.train.business.req.SkTokenQueryReq;
@@ -118,7 +119,7 @@ public class SkTokenService {
         LOG.info("会员【{}】获取日期【{}】车次【{}】的令牌开始", memberId, DateUtil.formatDate(date), trainCode);
 
              // 先获取令牌锁，再校验令牌余量，防止机器人抢票，lockKey就是令牌，用来表示【谁能做什么】的一个凭证
-             String lockKey =DateUtil.formatDate(date) + "-" + trainCode + "-" + memberId;
+             String lockKey = RedisKeyPreEnum.SK_TOKEN + "-" +DateUtil.formatDate(date) + "-" + trainCode + "-" + memberId;
              Boolean setIfAbsent = redisTemplate.opsForValue().setIfAbsent(lockKey, lockKey, 5, TimeUnit.SECONDS);
              if (Boolean.TRUE.equals(setIfAbsent)) {
                  LOG.info("恭喜，抢到令牌锁了！lockKey：{}", lockKey);
